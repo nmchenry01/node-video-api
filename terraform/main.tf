f2911056fcf2384948f83358be481816c8063e02
nmchenry01 <nicholas.mchenry@levvel.io>
2019-04-02 20:35:45 -0700

    Revert "added eslint to dev dependencies, lambda boilerplate for .js and .tf"
    This reverts commit 4f35f620672c1bc9d500864626a0c76fa4b3cd70.

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,23 +14,22 @@ module "iam" {
   source = "./modules/iam"
 }
 
-locals {
-  env     = "${terraform.workspace}"
-  tempDir = "../temp"
-}
-
 resource "aws_lambda_function" "get_video_data_lambda" {
-  filename      = "${local.tempDir}/get_video_data_lambda.zip"
-  function_name = "Get-Video-Data-${local.env}"
-  role          = ""                                           // TODO: Implement IAM role
-  handler       = "index.handler"
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
 
-  source_code_hash = "${filebase64sha256("${local.tempDir}/get_video_data_lambda.zip")}"
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
 
   runtime = "nodejs8.10"
-  timeout = 10
 
-  tags = {
-    "enviornment" = "${local.env}"
+  environment {
+    variables = {
+      foo = "bar"
+    }
   }
 }
