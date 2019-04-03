d2162e567e255beb19c55d770d92170d569b78bd
nmchenry01 <nicholas.mchenry@levvel.io>
2019-04-02 20:07:46 -0700

    linked IAM module, added scripts and temp directory, lambda name refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,27 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+resource "aws_lambda_function" "get_video_data_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+
+  runtime = "nodejs8.10"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
