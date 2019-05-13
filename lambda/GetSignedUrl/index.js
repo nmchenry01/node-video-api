110f1d0ca59f2b1252b83dd95bcbb028cad93a6c
nmchenry01 <nicholas.mchenry@levvel.io>
2019-05-13 15:57:22 -0700

    feat(GetSignedUrl): scaffolding for lambda

--- lambda/GetSignedUrl/index.js
+++ lambda/GetSignedUrl/index.js
@@ -1,7 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
+const AWS = require('aws-sdk');
+
+const logger = require('./helpers/logger');
+const manifest = require('./config/manifest');
+
+const s3 = new AWS.S3();
+const { bucket } = manifest;
+
 exports.handler = async (event, context) => {
-  console.info('Received event:', JSON.stringify(event, null, 2));
-  console.info(context);
+  logger.info(
+    { event: 'START', triggerEvent: event, context },
+    'Lambda execution started'
+  );
 
   const response = {
     statusCode: 200,
