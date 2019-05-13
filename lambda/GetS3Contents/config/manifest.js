39884d134d239efc46731c1ce98c1ed8d7ac596c
nmchenry01 <nicholas.mchenry@levvel.io>
2019-05-13 13:31:32 -0700

    feat(logLevel): dynamically choose logLevel based on environment

--- lambda/GetS3Contents/config/manifest.js
+++ lambda/GetS3Contents/config/manifest.js
@@ -1,9 +1,11 @@
-// TODO: Add level based off of NODE_ENV property (injected via Terraform)
-// TODO: Inject bucket in from terraform, using lambda env variable
-
 const manifest = {
-  logLevel: 'INFO',
-  bucket: 'node-video-api-content',
+  logLevel: {
+    [process.env.NODE_ENV]: 'INFO',
+    development: 'TRACE',
+    qa: 'DEBUG',
+    production: 'INFO',
+  }[process.env.NODE_ENV],
+  bucket: process.env.bucket,
 };
 
 module.exports = manifest;
