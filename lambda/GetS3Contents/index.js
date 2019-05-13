321b3d930649dd7a1ec02a1fb08219b57f99a742
nmchenry01 <nicholas.mchenry@levvel.io>
2019-05-13 10:43:51 -0700

    feat(formatting): add formatting to response body

--- lambda/GetS3Contents/index.js
+++ lambda/GetS3Contents/index.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 
 const logger = require('./helpers/logger');
 const { listS3Objects } = require('./helpers/aws');
-// const { formatBody } = require('./helpers/format');
+const { formatBody } = require('./helpers/format');
 const manifest = require('./config/manifest');
 
 const s3 = new AWS.S3();
@@ -19,12 +19,13 @@ exports.handler = async (event, context) => {
   const s3Objects = await listS3Objects(s3, bucket, logger);
   logger.info({ event: 'REC', s3Objects });
 
-  // const formattedBody = formatBody(s3Objects);
+  const formattedBody = formatBody(s3Objects);
+  logger.info({ event: 'REC', formattedBody });
 
   const response = {
     statusCode: 200,
     headers: {},
-    body: 'Hello World from the GetS3Contents lambda',
+    body: JSON.stringify(formattedBody),
     isBase64Encoded: false,
   };
   return response;
